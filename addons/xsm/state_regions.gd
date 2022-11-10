21812fd20ef1d2a371d488e8c6d700d34e99fe7e
sudzoof <sunjoshua2@gmail.com>
2022-11-10 18:58:07 -0500

    Godot automatically added a new line

--- addons/xsm/state_regions.gd
+++ addons/xsm/state_regions.gd
@@ -1,4 +1,3 @@
-# MIT LICENSE Copyright 2020-2021 Etienne Blanc - ATN
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
